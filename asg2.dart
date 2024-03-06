0661a78c88d2422dccc64508aec5f4cde081ccd3
user <josephmwangi.ma56@gmail.com>
2024-03-06 13:44:13 +0300

    feature signup working

---
+++ asg2.dart
@@ -0,0 +1,33 @@
+// Task 1
+int addTwo(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  print("addtwo: $addTwo");
+}
+
+// Task 2
+int subtractTwo(int a, int b) {
+  return a - b;
+}
+
+// Task 3
+int multiplyTwo(int a, int b) {
+  return a * b;
+}
+
+// Task 4
+double divideTwo(int a, int b) {
+  return a / b;
+}
+
+// Task 5
+int stringLength(String str) {
+  return str.length;
+}
+
+// Task 6
+dynamic getFirstElement(List list) {
+  return list[0];
+}
